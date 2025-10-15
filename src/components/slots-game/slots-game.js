d072816e47d812fa0a00a08a9c5d51ba8aedaa51
Jack <jo223se@student.lnu.se>
2025-10-15 11:55:27 +0200

    Add template to slots-game.js

--- src/components/slots-game/slots-game.js
+++ src/components/slots-game/slots-game.js
@@ -1,6 +1,14 @@
 const template = document.createElement('template')
 template.innerHTML = `
-  
+  <div class="container">
+    <h1></h1>
+    <main>
+      <div class="slot"></div>
+      <div class="slot"></div>
+      <div class="slot"></div>
+    </main>
+    <button></button>
+  </div>
 `
 
 customElements.define('slots-game',
