1e52322d39c523f35c33791b35b707577db5b726
Jack <jo223se@student.lnu.se>
2025-10-15 12:47:33 +0200

    Add to template in slots-game.js

--- src/components/slots-game/slots-game.js
+++ src/components/slots-game/slots-game.js
@@ -1,13 +1,44 @@
 const template = document.createElement('template')
 template.innerHTML = `
+  <style>
+    h1{
+      width: fit-content;
+    }
+    .container{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      border: solid 2px black;
+      width: 90vw;
+      height: 90vw;
+      background-color: aquamarine;
+    }
+    main{
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      gap: 5vw;
+      width: 90%;
+      height: 30%;
+      border: solid 2px black;
+      border-radius: 2em;
+      background-color: azure;
+    }
+    .slot{
+      width: 25%;
+      height: 75%;
+      background-color: grey;
+    }
+  </style>
   <div class="container">
-    <h1></h1>
+    <h1>Slot Game</h1>
     <main>
       <div class="slot"></div>
       <div class="slot"></div>
       <div class="slot"></div>
     </main>
-    <button></button>
+    <button>Spin</button>
   </div>
 `
 
