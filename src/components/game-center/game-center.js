70ebc7eeafb7db07af0f890830ade8a6ffbcc6af
Jack <jo223se@student.lnu.se>
2025-10-16 13:56:34 +0200

    Add slotsGame element to game-center.js

--- src/components/game-center/game-center.js
+++ src/components/game-center/game-center.js
@@ -1,3 +1,5 @@
+import '../slots-game/index.js'
+
 const template = document.createElement('template')
 template.innerHTML = `
   <div class="container"></div>
@@ -6,17 +8,19 @@ template.innerHTML = `
 customElements.define('game-center',
   class extends HTMLElement {
     #container
+    #slotsGame
     constructor () {
       super()
 
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
 
-      this.#container = this.querySelector('.container')
+      this.#container = this.shadowRoot.querySelector('.container')
+      this.#slotsGame = document.createElement('slots-game')
     }
 
     connectedCallback(){
-      this.#container.appendChild()
+      this.#container.appendChild(this.#slotsGame)
     }
   }
 )
