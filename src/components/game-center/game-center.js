e01d2de8c509eef8270f7e89d43ce28396b5d4a3
Jack <jo223se@student.lnu.se>
2025-10-16 13:51:37 +0200

    Add container element to game-center.js

--- src/components/game-center/game-center.js
+++ src/components/game-center/game-center.js
@@ -5,11 +5,14 @@ template.innerHTML = `
 
 customElements.define('game-center',
   class extends HTMLElement {
+    #container
     constructor () {
       super()
 
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
+
+      this.#container = this.querySelector('.container')
     }
   }
 )
