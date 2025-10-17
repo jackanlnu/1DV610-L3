f66a62987c2c81ff4aad85f10b0dfc3d635fc4a7
Jack <jo223se@student.lnu.se>
2025-10-17 13:31:15 +0200

    Add to template in game-center-home.js

--- src/components/game-center-home/game-center-home.js
+++ src/components/game-center-home/game-center-home.js
@@ -3,6 +3,12 @@ template.innerHTML = `
   <style>
     .container{
       display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 2vw;
+      padding: 2vw;
+      border: solid 1px black;
+      border-radius: 1em;
+      background-color: #a8f9ff;
     }
   </style>
   <div class="container">
