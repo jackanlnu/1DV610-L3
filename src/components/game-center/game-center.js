f3ae5db23404bce51c3b369250ce97a1c1590212
Jack <jo223se@student.lnu.se>
2025-10-17 14:58:04 +0200

    Add to template in game-center.js

--- src/components/game-center/game-center.js
+++ src/components/game-center/game-center.js
@@ -8,16 +8,25 @@ template.innerHTML = `
       display: flex;
       flex-direction: column;
       align-items: center;
+      gap: 2vw;
       margin: 0;
       width: 70vw;
       height: 50vw;
       border: solid 1px black;
       padding: 1em;
+      background-color: #ffffff;
     }
     header{
       display: flex;
       flex-direction: row;
       gap: 2vw;
+    }
+    #returnHomeButton{
+      background-color: #00dd0b;
+      color: white;
+      border: none;
+      border-radius: 1em;
+      box-shadow: 2px 2px 4px 0px #000000;
     }
     main{
       width: 80%;
