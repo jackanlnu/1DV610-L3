ac0407b436e522cdb7fb03d4d8b5f7cda3c3f3f7
Jack <jo223se@student.lnu.se>
2025-11-03 13:38:09 +0100

    Add slot to slots-slot.js

--- src/components/slots-slot/slots-slot.js
+++ src/components/slots-slot/slots-slot.js
@@ -2,11 +2,14 @@ import { template } from "./slots-game-template.js";
 
 customElements.define('slots-slot',
   class extends HTMLElement {
+    #slot
     constructor () {
       super()
 
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
+
+      this.#slot = this.shadowRoot.getElementById('slot')
     }
   }
 )
