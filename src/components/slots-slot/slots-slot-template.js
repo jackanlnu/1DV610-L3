c5e095d2558bcde83f1624b2c457e2ba2af80ef7
Jack <jo223se@student.lnu.se>
2025-11-03 12:53:59 +0100

    Add code to slots-slot-template.js

---
+++ src/components/slots-slot/slots-slot-template.js
@@ -0,0 +1,11 @@
+const template = document.createElement('template')
+template.innerHTML = `
+  <style>
+    #slot{
+      background-color: gray;
+    }
+  </style>
+  <div id="slot"></div>
+`
+
+export {template}
