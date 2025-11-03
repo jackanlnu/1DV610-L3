348d2fb9582b76bedf08cab51aea094535ff7aea
Jack <jo223se@student.lnu.se>
2025-11-03 12:47:04 +0100

    Add basic structure to slots-slot.js

---
+++ src/components/slots-slot/slots-slot.js
@@ -0,0 +1,10 @@
+customElements.define('slots-slot',
+  class extends HTMLElement {
+    constructor () {
+      super()
+
+      this.attachShadow({ mode: 'open' })
+        .appendChild(template.content.cloneNode(true))
+    }
+  }
+)
