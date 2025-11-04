d688febca61fe2790779bd18be799fc9d1f4245b
Jack <jo223se@student.lnu.se>
2025-11-04 12:56:38 +0100

    Add to slots-slot-template

--- src/components/slots-slot/slots-slot-template.js
+++ src/components/slots-slot/slots-slot-template.js
@@ -3,6 +3,14 @@ template.innerHTML = `
   <style>
     #slot{
       background-color: gray;
+      display: flex;
+      width: 1em;
+      height: 1em;
+      background-color: grey;
+      font-size: 10vw;
+      align-items: center;
+      justify-content: center;
+      border-radius: 0.5em;
     }
   </style>
   <div id="slot"></div>
