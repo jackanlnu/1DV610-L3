bd0180c34ee5004c4971a9b2833d1af6a78ed786
Jack <jo223se@student.lnu.se>
2025-12-04 10:46:15 +0100

    Add should add coins if win event is dispatched in game-center.test

--- tests/game-center.test.js
+++ tests/game-center.test.js
@@ -1,5 +1,27 @@
 import { gameCenter } from '../src/components/game-center/game-center.js'
 
-describe('', () => {
-  
+describe('game-center class, test suite', () => {
+
+  beforeEach(() => {
+    localStorage.clear();
+    localStorage.setItem('game-center-coins', JSON.stringify(100))
+    gameCenter = document.createElement('game-center')
+    document.body.appendChild(gameCenter)
+  })
+
+  afterEach(() => {
+    document.body.removeChild(gameCenter)
+  })
+
+  test('should add coins if win event is dispatched', () => {
+    const winEvent = new CustomEvent('win', {
+      detail: { amount: 50 },
+      bubbles: true,
+      composed: true
+    });
+    
+    gameCenter.dispatchEvent(winEvent)
+    
+    expect(localStorage.getItem('game-center-coins')).toBe('150')
+  })
 })
