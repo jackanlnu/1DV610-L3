6e863f7845eaabdcba4889729e1dc19ee1d4c34d
Jack <jo223se@student.lnu.se>
2025-12-08 09:17:39 +0100

    Add mock to slots-game.test

--- tests/slots-game.test.js
+++ tests/slots-game.test.js
@@ -2,6 +2,8 @@ import { jest } from '@jest/globals'
 
 import '../src/components/slots-game/index.js'
 
+jest.mock('../src/components/slots-slot/index.js')
+
 describe('slots-game class, test suite', () => {
 
 })
