8d180e4be08ca8a9fa1de56b2d6ff16227a248b2
Jack <jo223se@student.lnu.se>
2025-12-08 09:54:18 +0100

    Add beforeEach to slots-slot.test

--- tests/slots-slot.test.js
+++ tests/slots-slot.test.js
@@ -2,4 +2,9 @@ import '../src/components/slots-slot/index.js'
 
 describe('slots-slot class, test suite', () => {
   let slot
+
+  beforeEach(() => {
+    gameCenter = document.createElement('game-center')
+    document.body.appendChild(gameCenter)
+  })
 })
